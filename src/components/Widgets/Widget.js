1d8a3200b6d53afe33a747031656d5c2dcd6a304
Haruna Oseni <harunaoseni23@gmail.com>
2021-06-25 18:34:15 -0500

    screen viewport for mobile implemented

--- src/components/Widgets/Widget.js
+++ src/components/Widgets/Widget.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./Widget.css";
 
 const Widget = () => {
   return (
     <div className="widget">
       <iframe
+        className="widget"
         src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
         title="Facebook Page"
         width="340"
